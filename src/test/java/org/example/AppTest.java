1721b902cc8a6a48a9ff0956a863f9a3fc424f76
pierroTH <pierre.thouvenot@epsi.fr>
2021-11-20 12:00:51 +0100

    Second test passed

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -2,6 +2,7 @@ package org.example;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -17,11 +18,23 @@ public class AppTest
     {
         assertTrue( true );
     }
+    @Test
     public void isLeapYearsYearDivisibleBy400ShouldReturnTrue() {
         int year = 2000; //Arrange -> Année divisible par 400
         LeapYears leapYears = new LeapYears(); //Constructeur de la classe LeapYears
 //Nous avons besoin d'une instance de la classe LeapYears afin de pouvoir ap- peler la fonction isLeapYears
         boolean isLeapYears = leapYears.isLeapYears(year); //Act -> On regarde le ré- sultat avec l'année que nous avons renseignée durant l'étape Arrange.
-        assertTrue(isLeapYears); //Assert -> Le test est bon si isLeapYears est vrai.
+        Assert.assertTrue(isLeapYears); //Assert -> Le test est bon si isLeapYears est vrai.
     }
+    @Test
+    public void isLeapYearsYearDivisibleBy100ButNotBy400ShouldReturnFalse() {
+        //Arrange
+        int year = 1900;
+        LeapYears leapYears = new LeapYears();
+        //Act
+        boolean isLeapYears = leapYears.isLeapYears(year);
+        //Assert
+        Assert.assertFalse(isLeapYears);
+    }
+    
 }
