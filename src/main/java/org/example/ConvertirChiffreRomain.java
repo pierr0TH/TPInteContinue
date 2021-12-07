facda1cad5262bf9f3864252e50b6b11671ae06f
pierroTH <pierre.thouvenot@epsi.fr>
2021-12-07 11:38:42 +0100

    Modification de la fonction conversion chiffre romain pour passer le test sur les 10 premiers chiffres

--- src/main/java/org/example/ConvertirChiffreRomain.java
+++ src/main/java/org/example/ConvertirChiffreRomain.java
@@ -1,7 +1,27 @@
 package org.example;
 
 public class ConvertirChiffreRomain {
-    public String conversionChiffreArabeEnChiffreRomain (int number) {
-        throw new UnsupportedOperationException();
+    public static final int[] chiffresArabes = {1, 4, 5, 9, 10};
+    public static final String[] lettresRomaines = {"I", "IV", "V", "IX", "X"};
+    public String conversionChiffreArabeEnChiffreRomain (int nombre) {
+        String chiffreRomain = "";
+        if (nombre < 1 ) {
+            System.out.println("Nombre invalide pour la conversion en chiffre romain");
+        }
+        while (nombre > 0) {
+            if (nombre > 3000) {
+                System.out.println("Nombre invalide pour la conversion en chiffre romain");
+                break;
+            }
+            int num = 0;
+            for (int i = 0; i < chiffresArabes.length; i++) {
+                if (nombre >= chiffresArabes[i]) {
+                    num = i;
+                }
+            }
+            chiffreRomain += lettresRomaines[num];
+            nombre -= chiffresArabes[num];
+        }
+        return chiffreRomain;
     }
 }
