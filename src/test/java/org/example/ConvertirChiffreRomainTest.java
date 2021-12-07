25d03d15c0d1bfb304c21a058fb91a2c87d75ed1
pierroTH <pierre.thouvenot@epsi.fr>
2021-12-07 11:41:26 +0100

    Ajout de deux test pour la conversion en chiffre romains

--- src/test/java/org/example/ConvertirChiffreRomainTest.java
+++ src/test/java/org/example/ConvertirChiffreRomainTest.java
@@ -10,4 +10,18 @@ public class ConvertirChiffreRomainTest {
         String conversionChiffres = chiffreRomain.conversionChiffreArabeEnChiffreRomain(number);
         Assert.assertEquals("V", conversionChiffres);
     }
+    @Test
+    public void testConversionChiffreArabeEnChiffreRomain_doitRetournerMCMXLIV() {
+        int number = 1944;
+        ConvertirChiffreRomain chiffreRomain = new ConvertirChiffreRomain();
+        String conversionChiffres = chiffreRomain.conversionChiffreArabeEnChiffreRomain(number);
+        Assert.assertEquals("MCMXLIV", conversionChiffres);
+    }
+    @Test
+    public void testConversionChiffreArabeEnChiffreRomain_doitRetournerMMVI() {
+        int number = 2006;
+        ConvertirChiffreRomain chiffreRomain = new ConvertirChiffreRomain();
+        String conversionChiffres = chiffreRomain.conversionChiffreArabeEnChiffreRomain(number);
+        Assert.assertEquals("MCMXLIV", conversionChiffres);
+    }
 }
