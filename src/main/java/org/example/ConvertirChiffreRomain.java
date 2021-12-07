932ab4c2dede5ecbd5111277bead3746935af331
pierroTH <pierre.thouvenot@epsi.fr>
2021-12-07 11:45:36 +0100

    Ajout des autres chiffres arabes et lettres romaines dans les variables correspondantes pour passer les test

--- src/main/java/org/example/ConvertirChiffreRomain.java
+++ src/main/java/org/example/ConvertirChiffreRomain.java
@@ -1,8 +1,8 @@
 package org.example;
 
 public class ConvertirChiffreRomain {
-    public static final int[] chiffresArabes = {1, 4, 5, 9, 10};
-    public static final String[] lettresRomaines = {"I", "IV", "V", "IX", "X"};
+    public static final int[] chiffresArabes = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+    public static final String[] lettresRomaines = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
     public String conversionChiffreArabeEnChiffreRomain (int nombre) {
         String chiffreRomain = "";
         if (nombre < 1 ) {
