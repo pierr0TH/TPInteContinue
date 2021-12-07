2b87a0fc4a7c51eb226f1dda695cdab31a444b28
pierroTH <pierre.thouvenot@epsi.fr>
2021-12-07 10:34:52 +0100

    Changement dans la fonction saluer pour passer le test du cri

--- src/main/java/org/example/Bonjour.java
+++ src/main/java/org/example/Bonjour.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Bonjour {
+    private static boolean isStringUpperCase(String str){
+        char[] charArray = str.toCharArray();
+        for(int i=0; i < charArray.length; i++){
+            if( !Character.isUpperCase( charArray[i] ))
+                return false;
+        }
+        return true;
+    }
     public String saluer(String name) {
-        return name == null || name.equals("") ? "Bonjour, l'ami" : "Bonjour, " + name;
+        if (Objects.equals(name, "")) return "Bonjour, l'ami";
+        if (isStringUpperCase(name)) return "BONJOUR, " + name;
+        else return "Bonjour, " + name;
     }
 }
